df09e7ec037b3b71ed4fd8521bd642abb32930b9
Tehs <tehseinf@gmail.com>
2022-12-05 16:09:37 +0000

    gygyg

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+var sen =0;
 function load_JSON(){
     var xmlhttp = new XMLHttpRequest();
     var url = "senators.json";
@@ -6,7 +7,7 @@ function load_JSON(){
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             
             //Parse the JSON data to a JavaScript variable. 
-            var parsedObj = JSON.parse(xmlhttp.responseText);    
+            var parsedObj = JSON.parse(xmlhttp.responseText);  
             // This function is defined below and deals with the JSON data parsed from the file. 
             displayJSON(parsedObj); 
         }
@@ -19,7 +20,7 @@ var pen = JSON.parse(xmlhttp.responseText);
     
 function displayJSON(obj) {
     
-    var sen = obj.objects;
+     sen = obj.objects;
     
     // This code iterates through the colorArray and writes html code to put the color information in a table.
     var rep = 0;
@@ -85,7 +86,7 @@ function displayJSON(obj) {
         var gend = sen[i].person.gender; 
         var rank = sen[i].senator_rank_label; 
         
-        seninfo += "<tr><td><a href=\"index2.html\">"+ name + "</a></td><td>" + par + "</td><td>" + state + "</td><td>"+gend +"</td><td>"+rank+"</td><td>"+ "<button onClick=\"page("+sen[i].person.cspanid+")\">Click me</button></td></tr>";
+        seninfo += "<tr><td><a href=\"index2.html\">"+ name + "</a></td><td>" + par + "</td><td>" + state + "</td><td>"+gend +"</td><td>"+rank+"</td><td>"+ "<button onClick=\"page("+i+")\">Click me</button></td></tr>";
     }
     seninfo += "</table>";
      // Close the table element.
@@ -108,13 +109,13 @@ function page(num)
         window.location.href = "test.html";
         seninfo = "<table border = 2 >";
         seninfo += "<tr><th>Name</th><th>Party</th><th>State</th><th>Gender</th><th>Rank</th><th>but</th></tr>";
-        i=num
-        var name=pen[i].person.firstname + " "+sen[i].person.middlename + " "+sen[i].person.lastname ;
-        var par = pen[i].party;
+        i=num;
+        var name=sen[i].person.firstname + " "+sen[i].person.middlename + " "+sen[i].person.lastname ;
+        var par = sen[i].party;
         var state = sen[i].state;
         var gend = sen[i].person.gender; 
         var rank = sen[i].senator_rank_label;
-        seninfo += "<tr><td><a href=\"index2.html\">"+ name + "</a></td><td>" + par + "</td><td>" + state + "</td><td>"+gend +"</td><td>"+rank+"</td><td>"+ "<button onClick=\"page("+sen,sen[i].person.cspanid+")\">Click me</button></td></tr>";
+        seninfo += "<tr><td><a href=\"index2.html\">"+ name + "</a></td><td>" + par + "</td><td>" + state + "</td><td>"+gend +"</td><td>"+rank+"</td><td>"+ rank+"</td></tr>";
         seninfo += "</table>";
         document.getElementById("id05").innerHTML = seninfo;
 
