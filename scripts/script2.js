f8b292b7c41cc14bfeeb0f7dbc19820412019422
Mugesh SP <mushi025@gmail.com>
2022-12-05 08:28:51 +0000

    HTML CSS

--- scripts/script2.js
+++ scripts/script2.js
@@ -42,7 +42,7 @@ function displayJSON(obj) {
     var name;
     j=0
     var leadInfo = "";
-    leadInfo += "<thead><th>Type</th><th>Title</th><th>Party</th></thead>"; 
+    leadInfo += "<thead><tr><th>Name</th><th>Title</th><th>Party</th></tr></thead><tbody>"; 
     for (var i=0; i <sen.length; i++) 
     {    
          
@@ -51,18 +51,51 @@ function displayJSON(obj) {
         if(lead != null)    
         {
             var par = sen[i].party;
-            name=sen[i].person.firstname + " "+sen[i].person.middlename + " "+sen[i].person.lastname ;            
-            leadInfo += "<tr><td>" + name + "</td><td>" + lead + "</td><td>" + par + "</td></tr>";
-            j += 1;
+            if(par=="Democrat")
+            {
+                name=sen[i].person.firstname + " "+sen[i].person.middlename + " "+sen[i].person.lastname ;            
+                leadInfo += "<tr><td style>" + name + "</td><td>" +  lead+ "</td><td>" + par + "</td></tr>";
+            }
         }
         
-    }     // Close the table element.
+    }
+    for (var i=0; i <sen.length; i++) 
+    {    
+         
+        var lead = sen[i].leadership_title; 
+        
+        if(lead != null)    
+        {
+            var par = sen[i].party;
+            if(par=="Republican")
+            {
+                name=sen[i].person.firstname + " "+sen[i].person.middlename + " "+sen[i].person.lastname ;            
+                leadInfo += "<tr><td>" + lead + "</td><td>" +  name+ "</td><td>" + par + "</td></tr>";
+            }
+        }
+        
+    }  
+    leadInfo+= "</tbody></table>"   // Close the table element.
+    seninfo = "";
+    seninfo += "<thead><tr><th>Name</th><th>Party</th><th>State</th><th>Gender</th><th>Rank</th><th>but</th></tr></thead><tbody>";
+    for (var i=0; i <sen.length; i++) 
+    { 
+        var name=sen[i].person.firstname + " "+sen[i].person.middlename + " "+sen[i].person.lastname ;
+        var par = sen[i].party;
+        var state = sen[i].state;
+        var gend = sen[i].person.gender; 
+        var rank = sen[i].senator_rank_label; 
+        
+        seninfo += "<tr><td><a href=\"index2.html\">"+ name + "</a></td><td>" + par + "</td><td>" + state + "</td><td>"+gend +"</td><td>"+rank+"</td><td>"+ "<button onClick=\"page()\">Click me</button></td></tr>";
+    }
+    seninfo += "</tbody></table>";
     
     
     // Add the new html code to the div element with id = 'id01'.
     document.getElementById("democratic_count").innerHTML = dem;
     document.getElementById("republican_count").innerHTML = rep;
     document.getElementById("lead_role_info_table").innerHTML = leadInfo;
+    document.getElementById("senators_table").innerHTML = seninfo;
     
     
 }
